8991ceb3fa6aeb920f745c2240ab43e1230cea0a
fengmingdev <1028708571@qq.com>
2025-08-08 11:41:41 +0800

    Update minimum iOS version to 13.0 for all package managers

--- Package.swift
+++ Package.swift
@@ -1,25 +1,30 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "RouterKit",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .macOS(.v11),
+        .tvOS(.v14),
+        .watchOS(.v7)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RouterKit",
+            type: .dynamic,
+            targets: ["RouterKit"]),
+        .library(
+            name: "RouterKitStatic",
+            type: .static,
             targets: ["RouterKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,10 +32,14 @@ let package = Package(
         .target(
             name: "RouterKit",
             dependencies: [],
-            path: "Sources"),
+            path: "Sources",
+            resources: [.process("Documentation")],
+            publicHeadersPath: "include"),
         .testTarget(
             name: "RouterKitTests",
             dependencies: ["RouterKit"],
-            path: "Tests"),
-    ]
+            path: "Tests",
+            resources: [.copy("TestResources")]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
--- Podfile
+++ Podfile
@@ -1,15 +1,25 @@
 # Uncomment the next line to define a global platform for your project
 platform :ios, '13.0'
+platform :osx, '11.0'
+platform :tvos, '14.0'
+platform :watchos, '7.0'
 
 target 'RouterKit_Example' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for RouterKit_Example
-  pod 'RouterKit', :path => '.'
+  pod 'RouterKit', :path => '.', :modular_headers => true
 
   target 'RouterKit_Tests' do
     inherit! :search_paths
     # Pods for testing
+    pod 'XCTest-Gherkin', '~> 2.0'
   end
+end
+
+# 支持静态库
+target 'RouterKit_Static_Example' do
+  use_frameworks! :linkage => :static
+  pod 'RouterKit', :path => '.', :modular_headers => true
 end
